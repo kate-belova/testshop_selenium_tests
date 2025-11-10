17231862fe3bb6f7cb3ca7bbbae1636a9098185f
Kate Belova <thebrightestcry@gmail.com>
2025-11-10 21:07:31 +0300

    webdriver-manager removed

--- conftest.py
+++ conftest.py
@@ -2,8 +2,6 @@ from pathlib import Path
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 from pages import CartPage, MainPage, AlertPage, DesksPage, ProductPage
 
@@ -17,12 +15,9 @@ def pytest_configure(config):
 
 @pytest.fixture()
 def browser(request):
-    service = Service(executable_path=ChromeDriverManager().install())
-
     browser_options = webdriver.ChromeOptions()
     browser_options.page_load_strategy = 'eager'
     browser_options.add_argument('--headless=new')
-
     browser_options.add_argument('--window-size=1920,1080')
     browser_options.add_argument(
         '--disable-blink-features=AutomationControlled'
@@ -31,21 +26,19 @@ def browser(request):
         '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
         'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36'
     )
-
     browser_options.add_argument('--log-level=3')
     browser_options.add_argument('--disable-logging')
     browser_options.add_argument('--disable-dev-shm-usage')
     browser_options.add_argument('--no-sandbox')
     browser_options.add_argument('--disable-gpu-sandbox')
     browser_options.add_argument('--disable-software-rasterizer')
-
     browser_options.add_experimental_option(
         'excludeSwitches',
         ['enable-logging', 'enable-automation', 'ignore-certificate-errors'],
     )
     browser_options.add_experimental_option('useAutomationExtension', False)
 
-    browser = webdriver.Chrome(service=service, options=browser_options)
+    browser = webdriver.Chrome(options=browser_options)
     print('\nStart browser for test')
 
     yield browser
